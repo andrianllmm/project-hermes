3907ddba60300aa8388dd1cf554fae205c2eb89e
romyr <romyrlopez123@gmail.com>
2026-04-06 21:45:05 +0800

    chore: transfered to supabase seeds

--- supabase/seeds/incident_types.sql
+++ supabase/seeds/incident_types.sql
@@ -5,4 +5,5 @@ INSERT INTO public.incident_types (id, name, description) VALUES
   (gen_random_uuid(), 'Landslide', 'Sudden movement of soil, rock, or debris'),
   (gen_random_uuid(), 'Fire', 'Outbreak of fire in buildings, vehicles, or forests'),
   (gen_random_uuid(), 'Medical Emergency', 'Sudden illness or injury requiring attention'),
-  (gen_random_uuid(), 'Traffic Accident', 'Collisions or incidents on roads');
+  (gen_random_uuid(), 'Traffic Accident', 'Collisions or incidents on roads')
+ON CONFLICT (name) DO NOTHING;
